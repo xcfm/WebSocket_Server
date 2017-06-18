47c144f7bd7ca68edd62a103b227c2a9445432e5
xcfm <365326561@qq.com>
2017-06-18 09:06:32 +0800

    修复bug

--- src/main/java/com/fmz/websockets/WebSocketTest.java
+++ src/main/java/com/fmz/websockets/WebSocketTest.java
@@ -2,7 +2,9 @@ package com.fmz.websockets;
 
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.Map;
 
+import javax.net.ssl.ExtendedSSLSession;
 import javax.websocket.OnClose;
 import javax.websocket.OnError;
 import javax.websocket.OnMessage;
@@ -12,47 +14,52 @@ import javax.websocket.server.ServerEndpoint;
 
 @ServerEndpoint("/websocket")
 public class WebSocketTest {
-	private HashMap<String,Session> map =new HashMap<String,Session>();
+	private static HashMap<String, Session> map = new HashMap<String, Session>();
+	private Session session;
+
+	@OnOpen
+	public void onOpen(Session session) {
+		this.session = session;
+		map.put(session.getId(), session);
+		System.out.println("Client connected--------" + session.getId());
+	}
+
 	@OnMessage
-    public void onMessage(String message, Session session) 
-    	throws IOException, InterruptedException {
-		String id =session.getId();
-		if(id==null ||id.equals("")){
-			id ="12345";
-		}
+	public void onMessage(String message, Session session) throws IOException, InterruptedException {
 		// Print the client message for testing purposes
-		System.out.println("Received: " + message +"  From:"+id ); 
-		if(!map.containsKey(session.getId())){
-			map.put(session.getId(), session);
-		} 
-		// Send the ID to the client if it is register;
-		if(message.equals("reg")){ 
-			session.getBasicRemote().sendText("reg"+session.getId()); 
-		}else if(message.startsWith("login")){
-			String[] a =message.split("-");
-			if(a.length!=2){
-				return; 
-			}
-			Session sess=	map.get(a[1]);
-			if(sess==null)
-				return;
-			sess.getBasicRemote().sendText("loginSuccess");
+		System.out.println("Received: " + message + "  From:" + session.getId());
+		for (String key : map.keySet()) {
+			System.out.println("key= " + key + " and value= " + map.get(key));
 		}
-	
-		
-    }
+		// Send the ID to the client if it is register;
+		if (message.equals("reg")) {
+			session.getBasicRemote().sendText("reg" + session.getId());
+		} else if (message.startsWith("login")) {
+			String[] a = message.split("-");
+			if (a.length != 2) {
+				return;
+			}
+			Session loginSess = map.get(a[1]);
+			if (loginSess == null)
+				return;
+			loginSess.getBasicRemote().sendText("loginSuccess");
+			session.getBasicRemote().sendText("loginSuccess");
+		} else {
+			session.getBasicRemote().sendText("二维码已过期");
+		}
+
+	}
+
 	@OnError
 	public void error(Session session, Throwable t) {
-        /* Remove this connection from the queue */
-		map.remove(session.getId());
-    }
-	@OnOpen
-    public void onOpen (  ) {
-        System.out.println("Client connected"); 
-    }
+		if (session != null)
+			onClose(session);
+		System.out.println(t);
+	}
 
-    @OnClose
-    public void onClose () {
-    	System.out.println("Connection closed");
-    }
+	@OnClose
+	public void onClose(Session session) {
+		System.out.println("Connection closed");
+		map.remove(session.getId());
+	}
 }
